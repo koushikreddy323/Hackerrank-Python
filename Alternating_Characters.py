0aebe5b49112d54d611548043e3c03b37a910283
Koushik Reddy <koushikreddy323@gmail.com>
2020-09-01 19:21:53 +0530

    Create Alternating_Characters.py

---
+++ Alternating_Characters.py
@@ -0,0 +1,12 @@
+'''
+TO CALCULATE THE COUNT OF NO OF DELETIONS MADE TO GET A STRING WITHOUT SAME CHARS REPETING BESIDE
+'''
+def boom(s):
+  i=0
+  v=s[0]
+  for x in s[1:]:
+    if x==v:
+      i+=1
+    else:
+      v=x
+  print(i)
